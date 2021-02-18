bd9723a3ea298e7f673efd631976325718120b91
yamayamaKo <yamachanwow@gmail.com>
2021-02-18 13:22:32 +0900

    add:Quandlからデータを入手するコードを追加

---
+++ getQuandl/.ipynb_checkpoints/train-checkpoint.py
@@ -0,0 +1,20 @@
+import quandl
+
+# Effective Federal Funds Rate, Daily, 7-Day
+# DFF = quandl.get('FRED/DFF')
+# print(DFF)
+
+# Federal Fundsの上限と下限
+# DFEDTARU = quandl.get('DFEDTARU')
+# DFEDTARL = quandl.get('DFEDTARL')
+
+# これが新規失業保険申請件数 Weekly
+# ISCS = quandl.get("FRED/ICSA")
+# print(ISCA)
+
+# S&P500の株価 Daily
+# SP500 = quandl.get('SP500')
+
+# 月ごとのSP500の平均PER Monthly
+# SP500PE_MONTH = quandl.get("MULTPL/SP500_PE_RATIO_MONTH")
+# print(SP500PE_MONTH)
